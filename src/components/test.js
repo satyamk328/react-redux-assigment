74593d670506bef9ac10b326f453d2c62c4561d7
Satyam Kumar <satyamk328@gmail.com>
2020-06-02 00:03:27 +0530

    sfsfd

---
+++ src/components/test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LibraryList from './container/libraryList';
+
+describe("ComponentName", () => {
+  it("should render my component", () => {
+    const wrapper = shallow(<ComponentName />);
+  });
+});
